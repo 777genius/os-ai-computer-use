ef4b9c33df1ff491df11564b4385cff15fcf746e
777genius <777genius@users.noreply.github.com>
2025-10-24 21:52:32 +0000

    deploy: 830f5daafd81ff32bb8de7f100d1535ffdaac485

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3544701953"
+    serviceWorkerVersion: "906981316"
   }
 });
